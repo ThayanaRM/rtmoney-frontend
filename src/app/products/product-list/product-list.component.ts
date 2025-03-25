8d924d548ce52bf3d5932c85094ebc4cabaf9229
ThayanaRMachado <thayanarm@gmail.com>
2025-03-25 11:30:16 -0300

    feat(products) list products

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ConfirmationService, LazyLoadEvent, MessageService } from 'primeng/api';
+import { Table } from 'primeng/table';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { Pagination } from 'src/app/core/models/Pagination';
+import { Product } from 'src/app/core/models/Product';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  pagination: Pagination = new Pagination();
+
+  totalElements: number = 0;
+
+  filterName: string = '';
+
+  @ViewChild('characterTable') grid!: Table;
+
+  constructor(
+    private productService: ProductsService,
+  ) {
+    this.pagination.linesPerPage = 3;
+  }
 
   ngOnInit(): void {
+  }
+
+  list(page: number = 0): void {
+    this.pagination.page = page;
+    this.productService
+      .list(this.pagination, this.filterName)
+      .subscribe((data) => {
+        this.products = data.content;
+        this.totalElements = data.totalElements;
+      });
+  }
+
+  changePage(event: LazyLoadEvent) {
+    const page = event!.first! / event!.rows!;
+    this.list(page);
   }
 
 }
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pagination } from '../core/models/Pagination';
+import { Observable } from 'rxjs';
+import { AppConstants } from '../app-constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  list(pagination: Pagination, filterName: string): Observable<any> {
+    let params = new HttpParams()
+    .set('name', filterName)
+    .set('page', String(pagination.page))
+    .set('linesPerPage', String(pagination.linesPerPage))
+    .set('direction', String(pagination.direction))
+    .set('orderBy', String(pagination.orderBy));
+
+    return this.http.get<any>(AppConstants.backendServer + 'products', { params });
+  }
 }
